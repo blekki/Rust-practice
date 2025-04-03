da6bae672faaae9271b0421fb74d335972cf5f65
Orest <orest.chumak@gmail.com>
2025-04-03 19:16:27 +0300

    Update diagonals: Pre-final version

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,5 +1,5 @@
-const HEIGHT: i32 = 10;
-const WIDTH: i32 = 30;
+const HEIGHT: i32 = 17;
+const WIDTH: i32 = 43;
 
 
 fn main() {
@@ -7,7 +7,8 @@ fn main() {
     let correlation: f32 = WIDTH as f32 / HEIGHT as f32;
     // let correlation  = WIDTH / HEIGHT;'
 
-    let mut range = correlation;
+    // let mut range = correlation;
+    let mut range = 0.0;
 
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
@@ -29,12 +30,15 @@ fn main() {
             // let diagonal_one = (x as f32) >= (correlation * (HEIGHT - y - 1) as f32) &&
             //                          (x as f32) <  (correlation * (HEIGHT - y) as f32);
 
-            let diagonal_one = (x as f32) < (range) &&
-                                     (x as f32) >= (range - correlation);
+            // let diagonal_one = (x as f32) < (range) &&
+            //                          (x as f32) >= (range - correlation);
+            // let diagonal_two = (x as f32) < (WIDTH as f32 - range + correlation) &&
+            //                          (x as f32) >= (WIDTH as f32 - range);
 
-            let diagonal_two = (x as f32) < (WIDTH as f32 - range + correlation) &&
-                                     (x as f32) >= (WIDTH as f32 - range);
+            let diagonal_one = (x) == (range as i32);
+            let diagonal_two = (x) == (WIDTH - 1 - (range as i32));
 
+            // if diagonal_one {
             if diagonal_one || diagonal_two {
                 current_symbol = '*';
             }
