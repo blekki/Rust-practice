df6cebd7b9a7bac37ec5d2244ceebd8ad96dbfe8
Orest <orest.chumak@gmail.com>
2025-04-03 17:32:52 +0300

    Add first simple diagonal

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,23 +1,43 @@
-const HEIGHT: u32 = 6;
-const WIDTH: u32 = 10;
+const HEIGHT: i32 = 10;
+const WIDTH: i32 = 40;
 
 
 fn main() {
     
-    
-    for y in 1..=HEIGHT {
-        for x in 1..=WIDTH {
-            let is_hor = x == 1 || x == WIDTH;
-            let is_ver = y == 1 || y == HEIGHT;
+    let correlation: f32 = WIDTH as f32 / HEIGHT as f32;
+    // let correlation  = WIDTH / HEIGHT;
+
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
             
             let mut current_symbol = ' ';
+
+            let is_hor = x == 0 || x == WIDTH - 1;
+            let is_ver = y == 0 || y == HEIGHT - 1;
             if is_hor || is_ver {
                 current_symbol = '*';
             }
 
+            
+            let diagonal_one = (x as f32) >= correlation * (y as f32) && (x as f32) < correlation * ((y + 1) as f32);
+            if diagonal_one {
+                current_symbol = '*';
+            }
+            
             print!("{current_symbol}");
             
         }
         println!();
     }
+
+    // let mut str = String::from("");
+    // str.chars().nth(2).unwrap() = '2';
+    // for _ in 0..4 {
+    //     str.push('a');
+    // }
+    // println!("{str}");
+
+    // let mut sli  = "sdfsdf";
+    // sli.chars().;
+    // println!("{sli}");
 }
