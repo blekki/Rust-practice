f1df19ab4963f34b70850b011f0ce4589e41cf76
Orest <orest.chumak@gmail.com>
2025-04-07 17:13:49 +0300

    Add the comments and some structured everything

--- src/homework/hw04.rs
+++ src/homework/hw04.rs
@@ -1,22 +1,25 @@
 
 fn drawRhomb(width: i32, height: i32) -> ()
 {
-    let correlation: f32 = width as f32 / height as f32;
+    let correlation: f32 = (width as f32) / (height as f32);
 
+    // check every line and column
     for a in 0..height {
         for b in 0..width {
-            let mut current_symbol: char = '.';
+            let mut current_symbol: char = ' ';
             
+            // positions depend on a rhomb 
             let mut y = (a - (height / 2)).abs();
-            let mut x = (b - (width / 2)).abs();
-            x = (x as f32 / correlation) as i32;
+            let mut x = (b - (width  / 2)).abs();
+                    x = (x as f32 / correlation) as i32; // add a correlation to the "x" position
 
+            // check does position in the rhomb space
             let inside_rhomb = (x + y) < (height as f32 / 2.0).round() as i32;
-            
             if inside_rhomb {
-                current_symbol = '#';
+                current_symbol = '*';
             }
 
+            // print result
             print!("{current_symbol}");
         }
         println!();
