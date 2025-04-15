5fca07485ac74ac374161cf6a36f7bfd72de780e
Orest <orest.chumak@gmail.com>
2025-04-15 18:52:57 +0300

    Optimized code.

--- src/homework/hw05.rs
+++ src/homework/hw05.rs
@@ -1,21 +1,26 @@
 
 fn gcd(first_value: u32, second_value: u32) -> u32
 {
-    // pre-checking 
-    if first_value == second_value {
-        return first_value;
-    }
-    if first_value == 0 || second_value == 0 {
-        return 0;
-    }
-
     // try to find half of higher value
     let mut lower = first_value;
+    let mut higher= second_value;
     if second_value < first_value {
+        higher = first_value;
         lower = second_value;
     }
 
-    // check everything number lower then "half of higher value" 
+    // pre-checking
+    if lower == higher {
+        return lower;
+    }
+    if lower == 0 {
+        return 0;
+    }
+    if higher % lower == 0 {
+        return lower;
+    }
+
+    // check everything number lower then "half of lower value"
     let start_point: u32 = lower / 2; // isn't so necessary line (can be replaced in the next operator)
     for current_value in (1..start_point).rev() {
         let first_checking:  bool = first_value  % current_value == 0;
@@ -38,6 +43,13 @@ fn test() {
     println!("GCD of {:4} and {:4} = {:4}", 9, 3, gcd(9, 3));
     println!("GCD of {:4} and {:4} = {:4}", 56, 56, gcd(56, 56));
     println!("GCD of {:4} and {:4} = {:4}", 6790, 6797, gcd(6790, 6797));
+
+
+    println!("= {:4}", gcd(10, 2));
+    println!("= {:4}", gcd(1000000, 12312));
+    println!("= {:4}", gcd(2, 10));
+    println!("= {:4}", gcd(5, 10));
+    println!("= {:4}", gcd(15, 20));
 }
 
 //########## main program ##########
@@ -48,5 +60,5 @@ fn main()
 
     println!("GCD of {FIRST_NUMBER} and {SECOND_NUMBER} = {}", gcd(FIRST_NUMBER, SECOND_NUMBER));
 
-    // test();
+    test();
 }
