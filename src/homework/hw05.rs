d30eabbec48d4f4e07b1f96e334f43f0f726d35a
Orest <orest.chumak@gmail.com>
2025-04-15 18:24:16 +0300

    Get some optimized. Refresh higher value with lower.

--- src/homework/hw05.rs
+++ src/homework/hw05.rs
@@ -10,13 +10,13 @@ fn gcd(first_value: u32, second_value: u32) -> u32
     }
 
     // try to find half of higher value
-    let mut higher = first_value;
-    if second_value > first_value {
-        higher = second_value;
+    let mut lower = first_value;
+    if second_value < first_value {
+        lower = second_value;
     }
 
     // check everything number lower then "half of higher value" 
-    let start_point: u32 = higher / 2; // isn't so necessary line (can be replaced in next operator)
+    let start_point: u32 = lower / 2; // isn't so necessary line (can be replaced in the next operator)
     for current_value in (1..start_point).rev() {
         let first_checking:  bool = first_value  % current_value == 0;
         let second_checking: bool = second_value % current_value == 0;
@@ -29,15 +29,15 @@ fn gcd(first_value: u32, second_value: u32) -> u32
     return 1;
 }
 
-//#[test]
+//#[test #1]
 fn test() {
     println!("<><><> test results <><><>");
-    println!("GCB of {:4} and {:4} = {:4}", 12, 17, gcd(12, 17));
-    println!("GCB of {:4} and {:4} = {:4}", 3, 27,gcd(3, 27));
-    println!("GCB of {:4} and {:4} = {:4}", 44, 57, gcd(44, 57));
-    println!("GCB of {:4} and {:4} = {:4}", 9, 3, gcd(9, 3));
-    println!("GCB of {:4} and {:4} = {:4}", 56, 56, gcd(56, 56));
-    println!("GCB of {:4} and {:4} = {:4}", 6790, 6797, gcd(6790, 6797));
+    println!("GCD of {:4} and {:4} = {:4}", 12, 17, gcd(12, 17));
+    println!("GCD of {:4} and {:4} = {:4}", 3, 27,gcd(3, 27));
+    println!("GCD of {:4} and {:4} = {:4}", 44, 57, gcd(44, 57));
+    println!("GCD of {:4} and {:4} = {:4}", 9, 3, gcd(9, 3));
+    println!("GCD of {:4} and {:4} = {:4}", 56, 56, gcd(56, 56));
+    println!("GCD of {:4} and {:4} = {:4}", 6790, 6797, gcd(6790, 6797));
 }
 
 //########## main program ##########
