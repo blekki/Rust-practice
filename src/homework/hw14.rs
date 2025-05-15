31247ad7268fd0e9d9338ff13d6d6f04235330a5
Orest <orest.chumak@gmail.com>
2025-05-16 01:38:52 +0300

    Add a couple comments

--- src/homework/hw14.rs
+++ src/homework/hw14.rs
@@ -1,18 +1,23 @@
+// Grey code
 fn gray(len: u8) -> Vec<String> {
-    let mut result: Vec<String> = vec![];
-
+    let mut result: Vec<String> = vec![]; //result
+    
+    // prepare 
     let mut str = String::from("");
     let range = std::ops::Range::<usize> {start: 10 - (len as usize), end: 10};
     let mut current: u8 = 0b0;
     for _ in 0..(2 as i32).pow(len.into()) {
+        // convert binary value into string
         str = format!("{current:->10b}");
         result.push((str[range.clone()]).to_string().replace("-", "0"));
+        // next iter
         current += 1;
     }
 
     return result;
 }
 
+// ###### main program ######
 fn main() {
     let test_data = [
         (0, vec!("")),
@@ -23,10 +28,10 @@ fn main() {
         (4, vec!("0000", "0001", "0010", "0011", 
                  "0100", "0101", "0110", "0111", 
                  "1000", "1001", "1010", "1011",
-                 "1100", "1101", "1110", "1111")),
+                 "1100", "1101", "1110", "1111"))
     ];
 
     test_data.iter().for_each(|(n, out)| 
-            assert_eq!(gray(*n), *out)
-        );
+                              assert_eq!(gray(*n), *out)
+                             );
 }
