1c44608d51acf6a217f0f979923b5164f498762a
Orest <orest.chumak@gmail.com>
2025-05-22 17:57:56 +0300

    Update hw15.rs

--- src/homework/hw15.rs
+++ src/homework/hw15.rs
@@ -1,21 +1,21 @@
 use std::collections::HashMap;
 
-fn printer(array: &[String], unique: HashMap<char, u8>) {
-    array.iter().for_each(|s| {
-        s.chars().for_each(|ch| {
-            if unique.get(&ch).is_some() {
-                print!("{}", *unique.get(&ch).unwrap());
-            } else {
-                print!("-")
-            };
-        });
-        println!();
+// print one result row
+fn printer(str: String, unique: HashMap<char, u8>) {
+    str.chars().for_each(|ch| {
+        if unique.get(&ch).is_some() {
+            print!("{}", *unique.get(&ch).unwrap());
+        } else {
+            print!(" ")
+        };
     });
+    println!();
 }
 
 fn finder(array: &[String]) {
     let mut unique: HashMap<char, u8> = HashMap::new();
     let mut num: u8 = 1;
+
     array.iter().for_each(|s| {
         s.chars().for_each(|ch| {
             if ch != ' ' {
@@ -27,13 +27,18 @@ fn finder(array: &[String]) {
         });
     });
 
-    unique.iter().for_each(|f| {
-        print!("{} : ", f.0);
-        print!("{}", f.1);
-        println!();
-    });
+    // > if wanna check hashmap values
+    // unique.iter().for_each(|f| {
+    //     print!("{} : ", f.0);
+    //     print!("{}", f.1);
+    //     println!();
+    // });
 
-    printer(array, unique);
+    // print result
+    printer(array[0].clone(), unique.clone());
+    printer(array[1].clone(), unique.clone());
+    for _ in 0..array[0].len() { print!("-"); } println!(); // separater
+    printer(array[2].clone(), unique.clone());
 }
 
 fn main() {
@@ -43,9 +48,9 @@ fn main() {
                              String::from(" slon")];
 
     finder(&test);
-    // .iter().for_each(|f| {
-    //     print!("{} : ", f.0);
-    //     print!("{}", f.1);
-    //     println!();
-    // });
+    // <> must be the same result <>
+    //  1234
+    // 3   4
+    // -----
+    //  5678
 }
