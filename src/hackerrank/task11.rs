8d8fe497416780dfef259af35f29fad6460f6a12
Orest <orest.chumak@gmail.com>
2025-06-09 14:11:18 +0300

    Replace an old task into 11th

---
+++ src/hackerrank/task11.rs
@@ -0,0 +1,64 @@
+use std::io::{self, BufRead};
+
+fn countApplesAndOranges(s: i32, t: i32, a: i32, b: i32, apples: &[i32], oranges: &[i32]) {
+    let mut apples_in_area: u32 = 0;
+    let mut oranges_in_area: u32 = 0;
+    
+    // check apples
+    for apple in apples {
+        if (a + apple >= s) && (a + apple <= t) {
+            apples_in_area += 1;
+        }
+    }
+
+    // check oranges
+    for orange in oranges {
+        if (b + orange >= s) && (b + orange <= t) {
+            oranges_in_area += 1;      
+        }
+    }
+
+    // get results
+    println!("{apples_in_area}");
+    println!("{oranges_in_area}");
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let first_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let s = first_multiple_input[0].trim().parse::<i32>().unwrap();
+    let t = first_multiple_input[1].trim().parse::<i32>().unwrap();
+    let second_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let a = second_multiple_input[0].trim().parse::<i32>().unwrap();
+    let b = second_multiple_input[1].trim().parse::<i32>().unwrap();
+    let third_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let m = third_multiple_input[0].trim().parse::<i32>().unwrap();
+    let n = third_multiple_input[1].trim().parse::<i32>().unwrap();
+    let apples: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+    let oranges: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    countApplesAndOranges(s, t, a, b, &apples, &oranges);
+}
+
